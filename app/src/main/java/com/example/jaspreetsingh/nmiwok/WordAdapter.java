9413c808dab42949f47cb4392e2012e6ea58e46a
Jaspreet Singh (DELL) <cgc.jsbaidwan@gmail.com>
2017-07-01 18:28:02 +0530

    modify the wordAdapter to display image icon to the respective activity successfully

--- app/src/main/java/com/example/jaspreetsingh/nmiwok/WordAdapter.java
+++ app/src/main/java/com/example/jaspreetsingh/nmiwok/WordAdapter.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -43,15 +44,21 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         // Find the TextView in the list_item.xml layout with the ID miwok_text_view.
         TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
-        // Get the Miwok translation from the currentWord object and set this text on
+        // Get the Miwok translation from the current Word object and set this text on
         // the Miwok TextView.
         miwokTextView.setText(currentWord.getMiwokTranslation());
 
         // Find the TextView in the list_item.xml layout with the ID default_text_view.
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
-        // Get the default translation from the currentWord object and set this text on
+        // Get the default translation from the current Word object and set this text on
         // the default TextView.
         defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        // Find the ImageView in the list_item.xml layout with the ID list_item_icon
+        ImageView iconImageView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+        // Get the image resource ID from the current Word object and
+        // set the image icon view
+        iconImageView.setImageResource(currentWord.getImageResourceId());
 
         // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
         // the ListView.
