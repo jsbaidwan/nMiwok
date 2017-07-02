855c0e40c6500ff4e25b3db01096014328a18eca
Jaspreet Singh (DELL) <cgc.jsbaidwan@gmail.com>
2017-07-03 00:21:44 +0530

    modify the WordAdapter.java to check whether there is image or not. Working fine.

--- app/src/main/java/com/example/jaspreetsingh/nmiwok/WordAdapter.java
+++ app/src/main/java/com/example/jaspreetsingh/nmiwok/WordAdapter.java
@@ -56,9 +56,17 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         // Find the ImageView in the list_item.xml layout with the ID list_item_icon
         ImageView iconImageView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
-        // Get the image resource ID from the current Word object and
-        // set the image icon view
-        iconImageView.setImageResource(currentWord.getImageResourceId());
+
+        // Check if the if image is provided for this word or not
+        if(currentWord.hasImage()) {
+            // If image is available, display the provided image based on the resource ID
+            iconImageView.setImageResource(currentWord.getImageResourceId());
+            // Make sure the View is visible
+            iconImageView.setVisibility(View.VISIBLE);
+        }
+        else
+            // Otherwise hide the imageView (Set the visibility to GONE)
+            iconImageView.setVisibility(View.GONE);
 
         // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
         // the ListView.
